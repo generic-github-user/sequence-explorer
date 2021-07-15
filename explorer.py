699eb61e6358d0caf9e9a2acfe1a524858777db1
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-07-15 14:25:33 -0400

    Create explorer.py

---
+++ explorer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[11]:
+
+
+get_ipython().run_line_magic('matplotlib', 'widget')
+
+import numpy as np
+import matplotlib.pyplot as plt
+import string
+import random
